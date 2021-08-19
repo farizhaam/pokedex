4a50116b5c2d2fb35fad06a41c1febe1b20f25ca
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-08-19 17:54:01 +0200

    adding pokemons on pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = "Salmon Fillet";
-document.write(favoriteFood);
+pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Psyduck', height: 0.8, types: ['water']},
+    {name: 'Snorlax', height: 2.1, types: ['normal']},
+    {name: 'Gyarados', height: 6.5, types: ['water', 'flying']}
+];
