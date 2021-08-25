e2ca7594b616a128a7a7be4c19d74b723bd1e67f
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-08-25 15:35:55 +0200

    wrapping pokemonList inside pokemonRepository to avoid global state (IIFE)

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,47 @@
-//The Pokemon collection
-pokemonList = [
-    {
-        name: 'Bulbasaur', 
-        height: 0.7, 
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Psyduck', 
-        height: 0.8, 
-        types: ['water']
-    },
-    {
-        name: 'Snorlax', 
-        height: 2.1, 
-        types: ['normal']
-    },
-    {
-        name: 'Gyarados', 
-        height: 6.5, 
-        types: ['water', 'flying']
+//wrapping pokemonList inside pokemonRepository to avoid global state (IIFE)
+let pokemonRepository = (function(){
+    //The Pokemon collection
+    let pokemonList = [
+        {
+            name: 'Bulbasaur', 
+            height: 0.7, 
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Psyduck', 
+            height: 0.8, 
+            types: ['water']
+        },
+        {
+            name: 'Snorlax', 
+            height: 2.1, 
+            types: ['normal']
+        },
+        {
+            name: 'Gyarados', 
+            height: 6.5, 
+            types: ['water', 'flying']
+        }
+    ];
+
+    //add a single pokemon to the pokemonList
+    function add(pokemon){
+        pokemonList.push(pokemon);
     }
-];
 
-//the loop intitiates from 0, iterates until thge length of pokemonList, add +1 after each loop
-/* for (let i = 0; i < pokemonList.length; i++) {
-    
-    let reaction = ""; //initialize var reation
-    let pokemonHeight = pokemonList[i].height; //assigning value pokemonList.height into var pokemonHeight
-    
-    if (pokemonHeight < 1) {
-        reaction = "What a cutie!"; //reaction when the pokemonHeight is less than 1 m
-    } else if (pokemonHeight > 1 && pokemonHeight <= 5){
-        reaction = "This one is big."; //reaction when the pokemonHeight is between 1 and 5 m
-    } else {
-        reaction = "What an enormous pokemon!"; //reaction if the pokemonHeight is higher than 5 m
+    //return all pokemon from pokemonList
+    function getAll(){
+        return pokemonList;
     }
-    document.write(
-        pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - ' + reaction + '<br>'
-        //printing to HTML the pokemon's name and their height as well as the raction according to their height
-    );
 
-} */
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
 
-//iteration using forEach() function
-pokemonList.forEach(function(pokemon){
+//writing the content from pokemonRepository using forEach() function
+pokemonRepository.getAll().forEach(function(pokemon){
         
     let reaction = ""; //initialize var reation
     
